7f1ffa4c14be8dad656ebc2d937101909235cf3c
ashwin7573 <ashwinyadav7573@gmail.com>
2026-09-22 02:59:32 +0530

    Time: 23 ms (92.6%), Space: 82.8 MB (80.46%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -3,8 +3,10 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         int n = nums.size();
         sort(nums.begin(), nums.end());
-        for(int i=0; i<n-1; i++){
-            if(nums[i]==nums[i+1]) return true;
+        for(int i = 0; i<nums.size()-1; i++){
+            if(nums[i]==nums[i+1]){
+                return true;
+            }
         }
         return false;
     }
