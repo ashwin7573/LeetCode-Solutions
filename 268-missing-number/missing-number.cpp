5593b6a8342a764869d24a99e309fee3ed314417
ashwin7573 <ashwinyadav7573@gmail.com>
2026-09-22 16:47:33 +0530

    Time: 0 ms (100.00%) | Memory: 21.6 MB (93.67%) - LeetSync

--- 268-missing-number/missing-number.cpp
+++ 268-missing-number/missing-number.cpp
@@ -1,24 +1,12 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
+        int sum = 0;
         int n = nums.size();
-        
-        // Sort array: O(n log n)
-        sort(nums.begin(), nums.end());
-        
-        // Check if 0 is missing at the start
-        if (nums[0] != 0) {
-            return 0;
+        int x = (n*(n+1))/2 ;
+        for(int i=0; i<n; i++){
+            sum+=nums[i];
         }
-        
-        // Check for gaps between adjacent elements
-        for (int i = 0; i < n - 1; i++) {
-            if (nums[i + 1] - nums[i] != 1) {
-                return nums[i] + 1; // Return immediately when a gap is found
-            }
-        }
-        
-        // If 0 to n-1 are present, the missing number must be n
-        return n;
+        return x-sum;
     }
 };
